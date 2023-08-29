35fb503be281f1a5d92fb8cd5cf64788c302769b
ramin <rx.azhdari@gmail.com>
2023-08-29 08:47:57 +0200

    enh: only show extension on allowed websites

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,29 @@
+const ALLOWED_DOMAINS_REGEX = /twitter\.com|x\.com/i;
+
 const clickHandler = (_info, tab) => {
   chrome.tabs.sendMessage(tab.id, { func: "copyPostToClipboard" });
 };
 
-chrome.contextMenus.create({
-  id: "X_POST_TO_MARKDOWN_EXTENSION",
-  title: "Copy as Markdown",
-  contexts: ["page", "selection", "image", "link"],
-});
+const toggleContextMenus = (url) => {
+  if (!ALLOWED_DOMAINS_REGEX.test(url)) {
+    chrome.contextMenus.removeAll();
+    return;
+  }
 
-chrome.contextMenus.onClicked.addListener(clickHandler);
+  chrome.contextMenus.create({
+    id: "X_POST_TO_MARKDOWN_EXTENSION",
+    title: "Copy as Markdown",
+    contexts: ["page", "selection", "image", "link"],
+  });
+
+  chrome.contextMenus.onClicked.addListener(clickHandler);
+};
+
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  const tab = await chrome.tabs.get(activeInfo.tabId);
+  toggleContextMenus(tab.url);
+});
+chrome.tabs.onCreated.addListener((tab) => toggleContextMenus(tab.url));
+chrome.tabs.onUpdated.addListener((_tabId, _changeInfo, tab) =>
+  toggleContextMenus(tab.url)
+);
